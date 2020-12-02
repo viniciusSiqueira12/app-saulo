3730809f09aec598c855082566f5887d1cd10500
Marcos Vinicius <marcos@iMac-de-Marcos.local>
2020-12-01 22:38:22 -0300

    Primeiro commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb+srv://appsaullo:appsaulo@cluster0.ud79y.mongodb.net/teste?retryWrites=true&w=majority', { useNewUrlParser: true, 
+useUnifiedTopology: true, useCreateIndex: true});
+
+app.use("/files/",
+  express.static(path.resolve(__dirname, './tmp/uploads'))
+);
+
+requireDir('./src/models');
+app.use('/api',require('./src/routes'));
+
+app.listen(5000);
---
+++ src/models/Produto.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate');
+
+const ProdutoSchema = new mongoose.Schema({
+  Nome: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 100
+  },
+  Preco: {
+    type: Number,
+    required: true
+  },
+  Descricao: {
+    type: String,
+    required: true
+  },
+  Especificacao: {
+    type: String,
+    required: true
+  },
+  TipoRacao: {
+    type: String,
+    required: true
+  },
+  UrlFoto: {
+    type: String,
+    required: true
+  },
+  Usuario: { 
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Usuario' 
+  },
+  DataCriacao: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+ProdutoSchema.plugin(mongoosePaginate);
+mongoose.model('Produto', ProdutoSchema);
